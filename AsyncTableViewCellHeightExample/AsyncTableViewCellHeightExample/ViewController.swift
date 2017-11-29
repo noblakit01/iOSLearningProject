393cebb30cc19fab550c52e17719abb28e3e67cf
Luan Tran <noblakit01@gmail.com>
2017-11-29 22:07:22 +0700

    Update binding data.

--- AsyncTableViewCellHeightExample/AsyncTableViewCellHeightExample/ViewController.swift
+++ AsyncTableViewCellHeightExample/AsyncTableViewCellHeightExample/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        return cell
+    }
+    
 }
 
