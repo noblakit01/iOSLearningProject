57479958cb8fb4921c9edd86dc7562e867711da8
Luan Tran <noblakit01@gmail.com>
2017-11-29 21:46:17 +0700

    Create sample cell

---
+++ AsyncTableViewCellHeightExample/AsyncTableViewCellHeightExample/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  AsyncTableViewCellHeightExample
+//
+//  Created by Minh Luan Tran on 11/29/17.
+//  Copyright © 2017 Minh Luan Tran. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
