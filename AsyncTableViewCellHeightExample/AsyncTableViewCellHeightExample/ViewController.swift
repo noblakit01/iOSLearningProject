1506cc3b48c3ee80e8f6e1eeee20c8dbd2f20e58
Luan Tran <noblakit01@gmail.com>
2017-12-01 22:25:49 +0700

    Add urls data for example project

--- AsyncTableViewCellHeightExample/AsyncTableViewCellHeightExample/ViewController.swift
+++ AsyncTableViewCellHeightExample/AsyncTableViewCellHeightExample/ViewController.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import SwiftyImageCache
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    let urls: [String] = [
+        "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
+        "http://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg",
+        "https://ichef.bbci.co.uk/news/1024/cpsprodpb/693C/production/_95804962_p0517py6.jpg",
+        "http://www.petmd.com/sites/default/files/cat-lady-blog.jpg",
+        "http://www.lanlinglaurel.com/data/out/109/4898297-cat-picture.jpg"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,11 +30,14 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return urls.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        if let url = URL(string: urls[indexPath.row]) {
+            cell.photoImageView.setUrl(url)
+        }
         return cell
     }
     
