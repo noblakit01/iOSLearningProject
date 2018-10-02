cf7f9092e7a8d687fef9717d38fa4a41521540d9
Luan Tran <noblakit01@gmail.com>
2018-10-02 09:42:44 +0700

    Update examples source codes

--- RwSwiftReactiveProgrammingWithSwift/02_06_PerformSideEffects_start/RxSwift.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: Please build the scheme 'RxSwiftPlayground' first
-import XCPlayground
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
-
-import RxSwift
-
-
--- RwSwiftReactiveProgrammingWithSwift/02_06_PerformSideEffects_start/RxSwift.playground/Sources/SupportCode.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public func exampleOf(description: String, @noescape action: Void -> Void) {
-    print("\n--- Example of:", description, "---")
-    action()
-}
